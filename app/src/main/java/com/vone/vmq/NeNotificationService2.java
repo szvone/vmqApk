b28c230f0c03b0e926d81c7f0d53bb495b0a6ed7
zhou <875037306@qq.com>
2022-06-05 17:46:27 +0800

    fix bug

--- app/src/main/java/com/vone/vmq/NeNotificationService2.java
+++ app/src/main/java/com/vone/vmq/NeNotificationService2.java
@@ -30,7 +30,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class NeNotificationService2  extends NotificationListenerService {
+public class NeNotificationService2 extends NotificationListenerService {
     private String TAG = "NeNotificationService2";
     private String host = "";
     private String key = "";
@@ -41,28 +41,27 @@ public class NeNotificationService2  extends NotificationListenerService {
     //申请设备电源锁
     @SuppressLint("InvalidWakeLockTag")
     public void acquireWakeLock(Context context) {
-        if (null == mWakeLock)
-        {
-            PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
-            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK|PowerManager.ON_AFTER_RELEASE, "WakeLock");
-            if (null != mWakeLock)
-            {
+        if (null == mWakeLock) {
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "WakeLock");
+            if (null != mWakeLock) {
                 mWakeLock.acquire();
             }
         }
     }
+
     //释放设备电源锁
     public void releaseWakeLock() {
-        if (null != mWakeLock)
-        {
+        if (null != mWakeLock) {
             mWakeLock.release();
             mWakeLock = null;
         }
     }
+
     //心跳进程
-    public void initAppHeart(){
+    public void initAppHeart() {
         Log.d(TAG, "开始启动心跳线程");
-        if (newThread!=null){
+        if (newThread != null) {
             return;
         }
         acquireWakeLock(this);
@@ -70,7 +69,7 @@ public class NeNotificationService2  extends NotificationListenerService {
             @Override
             public void run() {
                 Log.d(TAG, "心跳线程启动！");
-                while (true){
+                while (true) {
 
                     SharedPreferences read = getSharedPreferences("vone", MODE_PRIVATE);
                     host = read.getString("host", "");
@@ -78,31 +77,32 @@ public class NeNotificationService2  extends NotificationListenerService {
 
                     //这里写入子线程需要做的工作
                     String t = String.valueOf(new Date().getTime());
-                    String sign = md5(t+key);
+                    String sign = md5(t + key);
 
 
                     OkHttpClient okHttpClient = new OkHttpClient();
-                    Request request = new Request.Builder().url("http://"+host+"/appHeart?t="+t+"&sign="+sign).method("GET",null).build();
+                    Request request = new Request.Builder().url("http://" + host + "/appHeart?t=" + t + "&sign=" + sign).method("GET", null).build();
                     Call call = okHttpClient.newCall(request);
                     call.enqueue(new Callback() {
                         @Override
                         public void onFailure(Call call, IOException e) {
                             final String error = e.getMessage();
-                            Handler handlerThree=new Handler(Looper.getMainLooper());
-                            handlerThree.post(new Runnable(){
-                                public void run(){
-                                    Toast.makeText(getApplicationContext() ,"心跳状态错误，请检查配置是否正确!"+error,Toast.LENGTH_LONG).show();
+                            Handler handlerThree = new Handler(Looper.getMainLooper());
+                            handlerThree.post(new Runnable() {
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "心跳状态错误，请检查配置是否正确!" + error, Toast.LENGTH_LONG).show();
                                 }
                             });
                         }
+
                         //请求成功执行的方法
                         @Override
                         public void onResponse(Call call, Response response) throws IOException {
-                            Log.d(TAG, "onResponse heard: "+response.body().string());
+                            Log.d(TAG, "onResponse heard: " + response.body().string());
                         }
                     });
                     try {
-                        Thread.sleep(30*1000);
+                        Thread.sleep(30 * 1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -113,8 +113,6 @@ public class NeNotificationService2  extends NotificationListenerService {
 
         newThread.start(); //启动线程
     }
-
-
 
 
     //当收到一条消息的时候回调，sbn是收到的消息
@@ -140,18 +138,22 @@ public class NeNotificationService2  extends NotificationListenerService {
                 Log.d(TAG, "**********************");
 
 
-                if (pkg.equals("com.eg.android.AlipayGphone")){
-                    if (content!=null && !content.equals("")) {
-                        if (content.indexOf("通过扫码向你付款")!=-1 || content.indexOf("成功收款")!=-1){
+                if (pkg.equals("com.eg.android.AlipayGphone")) {
+                    if (content != null && !content.equals("")) {
+                        if (content.contains("通过扫码向你付款") || content.contains("成功收款")
+                                || title.contains("通过扫码向你付款") || title.contains("成功收款")) {
                             String money = getMoney(content);
-                            if (money!=null){
+                            if (money == null) {
+                                money = getMoney(title);
+                            }
+                            if (money != null) {
                                 Log.d(TAG, "onAccessibilityEvent: 匹配成功： 支付宝 到账 " + money);
-                                appPush(2, Double.valueOf(money));
-                            }else {
-                                Handler handlerThree=new Handler(Looper.getMainLooper());
-                                handlerThree.post(new Runnable(){
-                                    public void run(){
-                                        Toast.makeText(getApplicationContext() ,"监听到支付宝消息但未匹配到金额！",Toast.LENGTH_SHORT).show();
+                                appPush(2, Double.parseDouble(money));
+                            } else {
+                                Handler handlerThree = new Handler(Looper.getMainLooper());
+                                handlerThree.post(new Runnable() {
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "监听到支付宝消息但未匹配到金额！", Toast.LENGTH_SHORT).show();
                                     }
                                 });
                             }
@@ -159,19 +161,19 @@ public class NeNotificationService2  extends NotificationListenerService {
 
                     }
 
-                }else if(pkg.equals("com.tencent.mm")){
+                } else if (pkg.equals("com.tencent.mm")) {
 
-                    if (content!=null && !content.equals("")){
-                        if (title.equals("微信支付") || title.equals("微信收款助手") || title.equals("微信收款商业版")){
+                    if (content != null && !content.equals("")) {
+                        if (title.equals("微信支付") || title.equals("微信收款助手") || title.equals("微信收款商业版")) {
                             String money = getMoney(content);
-                            if (money!=null){
-                                Log.d(TAG, "onAccessibilityEvent: 匹配成功： 微信到账 "+ money);
-                                appPush(1,Double.valueOf(money));
-                            }else{
-                                Handler handlerThree=new Handler(Looper.getMainLooper());
-                                handlerThree.post(new Runnable(){
-                                    public void run(){
-                                        Toast.makeText(getApplicationContext() ,"监听到微信消息但未匹配到金额！",Toast.LENGTH_SHORT).show();
+                            if (money != null) {
+                                Log.d(TAG, "onAccessibilityEvent: 匹配成功： 微信到账 " + money);
+                                appPush(1, Double.valueOf(money));
+                            } else {
+                                Handler handlerThree = new Handler(Looper.getMainLooper());
+                                handlerThree.post(new Runnable() {
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "监听到微信消息但未匹配到金额！", Toast.LENGTH_SHORT).show();
                                     }
                                 });
                             }
@@ -179,29 +181,30 @@ public class NeNotificationService2  extends NotificationListenerService {
                         }
                     }
 
-                }else if(pkg.equals("com.vone.qrcode")){
+                } else if (pkg.equals("com.vone.qrcode")) {
 
-                    if (content.equals("这是一条测试推送信息，如果程序正常，则会提示监听权限正常")){
-                        Handler handlerThree=new Handler(Looper.getMainLooper());
-                        handlerThree.post(new Runnable(){
-                            public void run(){
-                                Toast.makeText(getApplicationContext() ,"监听正常，如无法正常回调请联系作者反馈！",Toast.LENGTH_SHORT).show();
+                    if (content.equals("这是一条测试推送信息，如果程序正常，则会提示监听权限正常")) {
+                        Handler handlerThree = new Handler(Looper.getMainLooper());
+                        handlerThree.post(new Runnable() {
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "监听正常，如无法正常回调请联系作者反馈！", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
                 }
 
 
-
             }
         }
 
     }
+
     //当移除一条消息的时候回调，sbn是被移除的消息
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
 
     }
+
     //当连接成功时调用，一般在开启监听后会回调一次该方法
     @Override
     public void onListenerConnected() {
@@ -209,9 +212,9 @@ public class NeNotificationService2  extends NotificationListenerService {
         initAppHeart();
 
         Handler handlerThree = new Handler(Looper.getMainLooper());
-        handlerThree.post(new Runnable(){
-            public void run(){
-                Toast.makeText(getApplicationContext() ,"监听服务开启成功！",Toast.LENGTH_SHORT).show();
+        handlerThree.post(new Runnable() {
+            public void run() {
+                Toast.makeText(getApplicationContext(), "监听服务开启成功！", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -219,23 +222,20 @@ public class NeNotificationService2  extends NotificationListenerService {
     }
 
 
-
-
-
-    public void appPush(int type,double price){
+    public void appPush(int type, double price) {
         SharedPreferences read = getSharedPreferences("vone", MODE_PRIVATE);
         host = read.getString("host", "");
         key = read.getString("key", "");
 
-        Log.d(TAG, "onResponse  push: 开始:"+type+"  "+price);
+        Log.d(TAG, "onResponse  push: 开始:" + type + "  " + price);
 
         String t = String.valueOf(new Date().getTime());
-        String sign = md5(type+""+ price + t + key);
-        String url = "http://"+host+"/appPush?t="+t+"&type="+type+"&price="+price+"&sign="+sign;
-        Log.d(TAG, "onResponse  push: 开始:"+url);
+        String sign = md5(type + "" + price + t + key);
+        String url = "http://" + host + "/appPush?t=" + t + "&type=" + type + "&price=" + price + "&sign=" + sign;
+        Log.d(TAG, "onResponse  push: 开始:" + url);
 
         OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder().url(url).method("GET",null).build();
+        Request request = new Request.Builder().url(url).method("GET", null).build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
@@ -243,29 +243,31 @@ public class NeNotificationService2  extends NotificationListenerService {
                 Log.d(TAG, "onResponse  push: 请求失败");
 
             }
+
             @Override
             public void onResponse(Call call, Response response) throws IOException {
 
-                Log.d(TAG, "onResponse  push: "+response.body().string());
+                Log.d(TAG, "onResponse  push: " + response.body().string());
 
             }
         });
     }
 
-    public static String getMoney(String content){
+    public static String getMoney(String content) {
 
         List<String> ss = new ArrayList<String>();
-        for(String sss:content.replaceAll("[^0-9.]", ",").split(",")){
-            if (sss.length()>0)
+        for (String sss : content.replaceAll("[^0-9.]", ",").split(",")) {
+            if (sss.length() > 0)
                 ss.add(sss);
         }
-        if (ss.size()<1){
+        if (ss.size() < 1) {
             return null;
-        }else {
-            return ss.get(ss.size()-1);
+        } else {
+            return ss.get(ss.size() - 1);
         }
 
     }
+
     public static String md5(String string) {
         if (TextUtils.isEmpty(string)) {
             return "";
